68c9cb95fd31f4e22c9e285e4453479fbdd84939
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2022-09-29 00:22:55 +0600

    font awesome added & fack data create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 import Menubar from "./components/Menubar/Menubar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
 function App() {
   return (
     <div className="App">
       <Menubar></Menubar>
-      <h1 className="text-primary">hello people</h1>
+      <div className="container">
+        <h1 className="fs-2 mt-5 ">
+          <FontAwesomeIcon className="text-warning" icon={faDumbbell} />{" "}
+          Exercise Daily, Build Body
+        </h1>
+        <h5 className="mt-4">Choose your Exercise</h5>
+      </div> 
     </div>
   );
 }
