f09c11dc0b9726c9a70df656fc083631ddcdc3f0
KawsarCoder <96495318+KawsarCoder@users.noreply.github.com>
2022-09-29 23:37:54 +0600

    missing loop

--- src/components/Localdb/Localdb.js
+++ src/components/Localdb/Localdb.js
@@ -1,17 +1,16 @@
 import React from "react";
-// side bar link added 
+// side bar link added
 import Aside from "../Aside/Aside";
 
 const Localdb = ({ brakes, durations, brakeTimeClick }) => {
-  
   localStorage.setItem("defult", 0);
-  let timeDuration = localStorage.getItem("brake");
-  // local Storage value check 
+  // local Storage value check
   if (brakes) {
     localStorage.setItem("brake", brakes);
   } else {
     localStorage.setItem("brakes", brakes);
   }
+  let timeDuration = localStorage.getItem("brake");
   return (
     <div>
       {/* side var dynamic value form local storage  */}
